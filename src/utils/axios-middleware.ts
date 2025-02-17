7a915565fa521543a86f1b6420d01fc3c936726c
erdem tülü <erdemtulu@erdems-MacBook-Air.local>
2025-02-17 18:24:12 +0100

    fix: remove unnecessary headers from the interceptor

--- src/utils/axios-middleware.ts
+++ src/utils/axios-middleware.ts
@@ -3,13 +3,11 @@ import axios from 'axios';
 //Local Storage is used to store tokens since HttpOnly Cookies are not supported on the backend project
 const api = axios.create({
     baseURL: 'http://localhost:3000',
-    withCredentials: false // change this to true to use HttpOnly cookies
+    withCredentials: false 
 });
 api.interceptors.request.use(
     (config) => {
         config.headers["Content-Type"] = "application/json";
-        config.headers["Access-Control-Request-Headers"] = "*";
-        config.headers["Access-Control-Request-Method"] = "*";
         const accessToken = localStorage.getItem('accessToken') || '';
         if (accessToken) {
             config.headers['Authorization'] = `Bearer ${accessToken}`;
